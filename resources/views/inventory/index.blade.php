b68c71d8794d45663020b0086ca9ba8430be1d10
WasanPangpounju <friendlydev.net@gmail.com>
2025-08-22 11:58:12 +0700

    add po to order inventury

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -392,7 +392,7 @@
                                                         {{-- id', 'customerName', 'fabricId', 'fabricStructure', 'orderSumYard', 'purchaseOrder --}}
                                                     </td>
                                                     {{-- <td>{{ $withorders1->id }}::{{ $inventorySum->refId }}</td> --}}
-                                                    <td>po: {{ $withorders1->po }} / {{ $withorders1->customerName }}</td>
+                                                    <td>{{ $withorders1->customerName }}</td>
                                                     <td>{{ $withorders1->fabricId }}</td>
                                                     <td>{{ $withorders1->fabricStructure }}</td>
                                                     <td>{{ $withorders1->fabricPattern }}</td>
@@ -466,7 +466,7 @@
                                                 {{-- id', 'customerName', 'fabricId', 'fabricStructure', 'orderSumYard', 'purchaseOrder --}}
                                             </td>
                                             {{-- <td>{{ $withorders1->id }}::{{ $inventorySum->refId }}</td> --}}
-                                            <td>{{ $withorders1->customerName }}</td>
+                                            <td>po: {{ $withorders1->po }} / {{ $withorders1->customerName }}</td>
                                             <td>{{ $withorders1->fabricId }}</td>
                                             <td>{{ $withorders1->fabricStructure }}</td>
                                             <td>{{ $withorders1->fabricPattern }}</td>
