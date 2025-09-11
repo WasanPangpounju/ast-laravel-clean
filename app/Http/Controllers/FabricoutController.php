ae68c23e399a1e6551077f8b026446159bd3eb13
WasanPangpounju <friendlydev.net@gmail.com>
2025-09-11 14:04:25 +0700

    update

--- app/Http/Controllers/FabricoutController.php
+++ app/Http/Controllers/FabricoutController.php
@@ -798,7 +798,7 @@ class FabricoutController extends Controller
                 if (isset($records[0]->fabricStructReplace)) {
                     $this->fpdf->Cell(60, 5, iconv('UTF-8', 'cp874', 'รหัสผ้า Code : ' . $records[0]->fabricStructReplace . ' '), 0, 0);
                 } else {
-                    $this->fpdf->Cell(60, 5, iconv('UTF-8', 'cp874', 'รหัสผ้า Code : ' . $records[0]->fabricStruct . ' ' . ' #' . $records[0]->fabricW . ' \' ' . $records[0]->fabricPattern ), 0, 0);
+                    $this->fpdf->Cell(60, 5, iconv('UTF-8', 'cp874', 'รหัสผ้า Code : ' . $records[0]->fabricStruct . ' ' . ' #' . $records[0]->fabricW . ' \'\' ' . $records[0]->fabricPattern ), 0, 0);
                     // $this->fpdf->Cell(60, 5, iconv('UTF-8', 'cp874', 'รหัสผ้า : ' . $records[0]->fabricStructReplace . ' '), 0, 0);
                 }
                 $this->fpdf->Cell(60, 10, '', 0, 0);
