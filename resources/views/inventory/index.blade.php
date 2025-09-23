12b31f04a2fdeb60c798b09c4a9e821ddcb2f3d6
WasanPangpounju <friendlydev.net@gmail.com>
2025-09-23 07:03:27 +0700

    update table

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -402,7 +402,7 @@
                                                     <?php $check = 0; ?>
                                                     @for ($z = 0; $z < count($fabricoutdata2); $z++)
                                                         @if ($fabricoutdata2[$z]->purchaseOrder == $withorders1->id)
-                                                            <td>{{ $fabricoutdata2[$z]->fabric_w }}
+                                                            <td>{{ $fabricoutdata2[$z]->fabric_w }}</td>
                                                                 <?php $check = 1; ?>
                                                         @endif
                                                     @endfor
@@ -478,7 +478,7 @@
                                             <?php $check = 0; ?>
                                             @for ($z = 0; $z < count($fabricoutdata2); $z++)
                                                 @if ($fabricoutdata2[$z]->purchaseOrder == $withorders1->id)
-                                                    <td>{{ $fabricoutdata2[$z]->fabric_w }}
+                                                    <td>{{ $fabricoutdata2[$z]->fabric_w }}<td>
                                                         <?php $check = 1; ?>
                                                 @endif
                                             @endfor
