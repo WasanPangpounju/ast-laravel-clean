ef20cb9c7f7e131750541472037877822b4609e4
WasanPangpounju <friendlydev.net@gmail.com>
2025-09-26 00:33:32 +0700

    update inventory

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -321,9 +321,9 @@ print_r($request->input);
     $select_search = '';
     $searchInput   = '';
 
-    return view('inventory.index', compact(
-        'importorder','select_search','inventorydata','fabricoutdata','fabricoutdata2','orders'
-    ));
+    // return view('inventory.index', compact(
+    //     'importorder','select_search','inventorydata','fabricoutdata','fabricoutdata2','orders'
+    // ));
 }
 
         //check next data then save and set end count to session  and show create with end count
