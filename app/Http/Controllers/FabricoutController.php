6a0c1d2229281499434f6a59c9ec4843c6092a97
WasanPangpounju <friendlydev.net@gmail.com>
2025-09-26 00:43:47 +0700

    update fabricout

--- app/Http/Controllers/FabricoutController.php
+++ app/Http/Controllers/FabricoutController.php
@@ -399,7 +399,7 @@ class FabricoutController extends Controller
                     }
                 }
             }
-print_r($request);
+print_r($request->input);
             // return view('fabricout.create', compact('customers', 'order_id', 'customer_name', 'fabric_struct', 'orders', 'stockFabricStruct', 'vatA', 'vatB', 'vatC'));
         }
 
