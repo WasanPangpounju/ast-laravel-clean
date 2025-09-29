841a1af3a8251510293cb019ab5165d12f8311ae
WasanPangpounju <friendlydev.net@gmail.com>
2025-09-30 05:43:11 +0700

    update fabricout

--- app/Http/Controllers/FabriccheckController.php
+++ app/Http/Controllers/FabriccheckController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Support\Facades\Schema;
 use Illuminate\Http\Request;
 
 use App\Models\fabricout;
