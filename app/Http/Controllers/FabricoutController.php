723658f7fffd3ce7a7956a440ba650ec2524a730
WasanPangpounju <friendlydev.net@gmail.com>
2025-10-01 06:16:28 +0700

    update fabricout

--- app/Http/Controllers/FabricoutController.php
+++ app/Http/Controllers/FabricoutController.php
@@ -233,7 +233,7 @@ public function create()
 
     // 👉 เปลี่ยนมาใช้ตัวเลือกแบบเบาเครื่อง
 $stockLots = $this->stockPickerOptions();
-print_r(stockPickerOptions);
+print_r($this->stockPickerOptions);
 return view('fabricout.create', compact(
     'customers','order_id','customer_name','fabric_struct',
     'orders','vatA','vatB','vatC',
