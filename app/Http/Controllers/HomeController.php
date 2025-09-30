06c62f688e7a52764ec5139a9794c0bd6e1d54cc
WasanPangpounju <friendlydev.net@gmail.com>
2025-09-30 07:23:25 +0700

    update test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,11 +21,8 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index() {
-    return view('home-lite'); // ไฟล์ที่เป็น HTML เปล่า ๆ
-}
 
-    public function index_back()
+    public function index()
     {
         return view('home');
     }
