0fb9672fb3df02bec39ad2c751aee73d299e0e4c
WasanPangpounju <friendlydev.net@gmail.com>
2025-10-06 10:15:35 +0700

    update fabriccheck

--- resources/views/fabriccheck/show.blade.php
+++ resources/views/fabriccheck/show.blade.php
@@ -63,7 +63,7 @@
         </form>
 
         {{-- ปุ่มกลับ --}}
-        <a href="{{ route('fabriccheck.index') }}" class="btn btn-danger btn-sm">กลับ</a>
+        <a href="{{ route('fabriccheck.index') }}" class="btn b_order">กลับ</a>
       </div>
     </div>
   </div>
