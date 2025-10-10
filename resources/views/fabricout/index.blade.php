d55cba67f415cbbe4f7bcf0c4c939332723487aa
WasanPangpounju <friendlydev.net@gmail.com>
2025-10-11 06:37:12 +0700

    update

--- resources/views/fabricout/index.blade.php
+++ resources/views/fabricout/index.blade.php
@@ -165,12 +165,11 @@
                                             <td>
   <form action="{{ route('fabricout.store') }}" method="post" target="_blank">
     @csrf
-    <input type="text" name="ref_id" value="{{ $findFabric->refId }}">
+    <input type="hidden" name="ref_id" value="{{ $findFabric->refId }}">
     <button type="submit" class="btn b_order" name="submit" value="submitfabricout">
       สั่งใบส่ง
     </button>
   </form>
-
                                                 <form action="{{ route('fabricout.store') }}" method="post"
                                                     target="_blank">
                                                     @csrf
