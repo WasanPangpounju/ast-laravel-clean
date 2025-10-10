f268baa83169220dd860cef70496f47769ff49ab
WasanPangpounju <friendlydev.net@gmail.com>
2025-10-10 09:43:00 +0700

    update fabricimport

--- app/Models/fabricimport.php
+++ app/Models/fabricimport.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class fabricimport extends Model
+class FabricImport extends Model
 {
     use HasFactory;
 
