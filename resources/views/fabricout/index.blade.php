629ad4b97b24b91d003382f55fecf9d5125715b8
WasanPangpounju <friendlydev.net@gmail.com>
2025-10-11 06:43:38 +0700

    update

--- resources/views/fabricout/index.blade.php
+++ resources/views/fabricout/index.blade.php
@@ -165,7 +165,7 @@
                                             <td>
   <form action="{{ route('fabricout.store') }}" method="post" target="_blank">
     @csrf
-    <input type="hidden" name="ref_id" value="{{ $findFabric->refId }}">
+    <input type="text" name="ref_id" value="{{ $findFabric->refId }}">
     <button type="submit" class="btn b_order" name="submit" value="submitfabricout">
       สั่งใบส่ง
     </button>
@@ -254,6 +254,14 @@
                                             <td>{{ $sumfabricout[$i]->sumYardSum }}</td>
 
                                                                                         <td>
+                                                                                              <form action="{{ route('fabricout.store') }}" method="post" target="_blank">
+    @csrf
+    <input type="text" name="ref_id" value="{{ $findFabric->refId }}">
+    <button type="submit" class="btn b_order" name="submit" value="submitfabricout">
+      สั่งใบส่ง
+    </button>
+  </form>
+
                                                 <form action="{{ route('fabricout.store') }}" method="post"
                                                     target="_blank">
                                                     @csrf
