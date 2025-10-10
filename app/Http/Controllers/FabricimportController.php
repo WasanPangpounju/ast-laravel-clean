089bc8eeb0c302a57602103af040e422781eb5df
WasanPangpounju <friendlydev.net@gmail.com>
2025-10-10 09:54:51 +0700

    update fabricimport

--- app/Http/Controllers/FabricimportController.php
+++ app/Http/Controllers/FabricimportController.php
@@ -5,9 +5,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 use App\Models\FabricImport;
-<!-- use App\Models\customer; -->
-<!-- use App\Models\astpurchaseorder; // สำหรับ datalist เดิม -->
-<!-- use App\Models\fabricast;        // เอา fabric_w ประกอบ (ถ้าใช้) -->
+use App\Models\customer;
+//use App\Models\astpurchaseorder; // สำหรับ datalist เดิม
+//use App\Models\fabricast;        // เอา fabric_w ประกอบ (ถ้าใช้)
 
 class FabricImportController extends Controller
 {
